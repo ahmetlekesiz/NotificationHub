108b2c8d476b0cc88c08e38c7e3c8e9cde835917
Ahmet Lekesiz <ahmetlekesiz@gmail.com>
2020-09-10 01:29:34 +0300

    code refactoring on MessageSender.

--- src/main/java/MessagePackage/MessageSender.java
+++ src/main/java/MessagePackage/MessageSender.java
@@ -10,36 +10,30 @@ public class MessageSender {
 
     public boolean checkBlackListed(Company company, MessagePackage messagePackage){
         LocalDate currentDate = LocalDate.now();
-
         long noOfDaysBetween;
+
         if(messagePackage.getPackageInvoice().isPaidBefore()){
             noOfDaysBetween = ChronoUnit.DAYS.between(messagePackage.getPackageInvoice().getLastPaymentDate(), messagePackage.getPackageInvoice().getRecordDate());
-        }else{
+        } else{
             noOfDaysBetween = ChronoUnit.DAYS.between(currentDate, messagePackage.getPackageInvoice().getRecordDate());
         }
 
         if (noOfDaysBetween > 30) company.setBlackListed(true);
-
         if (company.isBlackListed()){
             System.out.println(company.getCompanyLanguage().getErrorMessage());
             return true;
-        }else{
-            return false;
         }
+        return false;
     }
 
     public boolean sendMessage(Company company, MessagePackage messagePackage) {
         if (checkBlackListed(company, messagePackage)) return false;
-        if (messagePackage.getCurrentMessageCount() < messagePackage.getPackageQuota()) {
-            System.out.println(messagePackage.getMessageContent(company.getCompanyLanguage()));
-            messagePackage.incrementMessageCount();
-            return true;
-        } else {
-            System.out.println(messagePackage.getMessageContent(company.getCompanyLanguage()));
+        if (messagePackage.getCurrentMessageCount() >= messagePackage.getPackageQuota()) {
             messagePackage.getPackageInvoice().setAmount(messagePackage.getPackageInvoice().getAmount() + messagePackage.getIncrementPrice());
             if (!messagePackage.isFlexible()) messagePackage.setCurrentMessageCount(0);
-            messagePackage.incrementMessageCount();
-            return true;
         }
+        messagePackage.incrementMessageCount();
+        return true;
     }
+
 }
